baa1fb20bdb385dd63a19401a0a1e9598695de07
Neharik Umashankar <neharik.umashankar@kpmg.co.uk>
2023-01-11 14:27:55 +0000

    Finished task 7

--- src/components/IndividualReview.jsx
+++ src/components/IndividualReview.jsx
@@ -3,17 +3,20 @@ import { useParams } from "react-router";
 import { getSingleReview } from "../api";
 import { Link } from "react-router-dom";
 import Comments from "./Comments";
-
+import { patchVotes } from "../api";
 
 const IndividualReview = () => {
   const { review_id } = useParams();
   const [singleReviewLoad, setSingleReviewLoad] = useState(true);
   const [singleReview, setSinglereview] = useState(undefined);
+  const [votes, setVotes] = useState(0);
+  const [err, setErr] = useState(null);
 
   useEffect(() => {
     setSingleReviewLoad(true);
     getSingleReview(review_id).then((info) => {
       setSinglereview(info.data.review);
+      setVotes(info.data.review.votes);
       setSingleReviewLoad(false);
     });
   }, [review_id]);
@@ -21,8 +24,18 @@ const IndividualReview = () => {
   if (singleReviewLoad) return <p>Review loading....</p>;
 
   if (singleReview !== undefined) {
+    const handleVote = (x) => {
+      setVotes(votes + x);
+      patchVotes(x, review_id).catch((err) => {
+        setVotes(votes - x);
+        setErr("An error has occurred, please try again.");
+      });
+    };
+
     const voteMessage =
-      singleReview.votes === 1 ? "1 vote" : `${singleReview.votes} votes`;
+      Math.abs(votes) === 1 ? `${votes} vote` : `${votes} votes`;
+
+    if (err) return <p>{err}</p>;
 
     return (
       <div>
@@ -45,10 +58,21 @@ const IndividualReview = () => {
         <br></br>
         <br></br>
         <h3>{voteMessage}</h3>
+        <button
+          onClick={() => {
+            handleVote(1);
+          }}
+        >
+          ⬆
+        </button>{" "}
+        <button
+          onClick={() => {
+            handleVote(-1);
+          }}
+        >
+          ⬇
+        </button>{" "}
         <Comments propsID={singleReview.review_id}></Comments>
-        <Link to={`/reviews/${review_id}/comments`}>
-          <p>View comments in fullscreen</p>
-        </Link>
       </div>
     );
   }
